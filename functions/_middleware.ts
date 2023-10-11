7566efc6bb57189afb25f76cf5ab5553ffb3f533
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2023-10-11 09:08:23 +0000

    fix: simplyfied script

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,16 +3,13 @@ import type {EventContext} from "@cloudflare/workers-types/2023-07-01"
 export async function onRequest(context: EventContext<any, any, any>) {
     const url = new URL(context.request.url);
     const pathname: string = url.pathname;
-    if (pathname.split('/').some(segment => segment.startsWith('.'))) {
+
+    // Allow only requests to /fedora/* and /kubernetes/* excluding any requests containing ..
+    if (!(pathname.startsWith('/fedora/') || pathname.startsWith('/kubernetes/')) || pathname.includes('..')) {
         return new Response(null, {
             status: 404
         });
     }
-    // Block access to self
-    if (pathname.indexOf("_middleware.ts") > 0){
-        return new Response(null, {
-            status: 404
-        });
-    }
+
     return await context.next()
 }
