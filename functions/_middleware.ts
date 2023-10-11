0f0f313df96ac8e484f148be3e92c05758d4694c
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2023-10-11 09:01:34 +0000

    fix: block access to dotfiles

---
+++ functions/_middleware.ts
@@ -0,0 +1,12 @@
+import type {EventContext} from "@cloudflare/workers-types/2023-07-01"
+
+export async function onRequest(context: EventContext<any, any, any>) {
+    const url = new URL(context.request.url);
+    const pathname: string = url.pathname;
+    if (pathname.split('/').some(segment => segment.startsWith('.'))) {
+        return new Response(null, {
+            status: 404
+        });
+    }
+    return await context.next()
+}
